22b728c354a7fcb56a00e17dbd8cdd7c1068e99f
Jess Green <2304490+jesgs@users.noreply.github.com>
2019-07-16 14:51:12 -0500

    Update .aliases.bash

--- .aliases.bash
+++ .aliases.bash
@@ -1,4 +1,4 @@
 alias la="ls -la"
-alias copypwd="pwd | pbcopy"
-alias mergesql="cat *.sql > dump.sql"
-alias homestead-init="composer require laravel/homestead"
+alias copypwd="pwd | pbcopy" # Useful if I can remember the stupid alias
+alias mergesql="cat *.sql > dump.sql" # Takes a bunch of small SQL files in a directory and batch them
+alias homestead-init="composer require laravel/homestead" # Once again, useful alias if it can be remembered
